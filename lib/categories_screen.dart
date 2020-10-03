536c27a9460a07689718cd38cf3e73fddcf1f006
Pedro <pedromoratelli@gmail.com>
2020-10-03 20:29:49 -0300

    Create category screen, list and list items

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 
+import './category_item.dart';
+import './dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (category) => CategoryItem(
+                category.title,
+                category.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
