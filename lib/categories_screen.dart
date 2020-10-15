f492c89b9f92366379e45c9ec89f84011ccd7677
Pedro Moratelli <pedromoratelli@gmail.com>
2020-10-15 15:17:22 -0300

    Refactor routes with Named Routes

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -15,6 +15,7 @@ class CategoriesScreen extends StatelessWidget {
         children: DUMMY_CATEGORIES
             .map(
               (category) => CategoryItem(
+                category.id,
                 category.title,
                 category.color,
               ),
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 import 'category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
   void _selectCategory(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) {
-        return CategoryMealsScreen();
-      }),
+    Navigator.of(context).pushNamed(
+      '/category-meals',
+      arguments: {
+        'id': id,
+        'title': title,
+      },
     );
   }
 
